dc1dd00914be054fbdfe594aea87d6e516f36820
jojomi <dev.jojomi@yahoo.com>
2024-03-21 03:05:45 +0100

    Add SSHCommand

--- ssh_command.go
+++ ssh_command.go
@@ -1,6 +1,36 @@
 package gorun
 
+import "fmt"
+
 type SSHCommand struct {
+	alias         string
 	options       map[string]any
 	remoteCommand Command
+}
+
+func NewSSHCommandFrom(alias string, command Command) *SSHCommand {
+	cmd := &SSHCommand{
+		alias:         alias,
+		remoteCommand: command,
+	}
+	return cmd
+}
+
+func (x SSHCommand) String() string {
+	return fmt.Sprintf(`ssh "%s" -- %s`, x.alias, x.remoteCommand.String())
+}
+
+func (x SSHCommand) Binary() string {
+	return "ssh"
+}
+
+func (x SSHCommand) Args() []string {
+	args := []string{
+		x.alias,
+		`--`,
+		x.remoteCommand.Binary(),
+	}
+
+	args = append(args, x.remoteCommand.Args()...)
+	return args
 }
